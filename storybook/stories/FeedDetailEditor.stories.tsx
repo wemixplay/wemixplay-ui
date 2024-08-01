9a689a89e2648dd3dea37bcb083828399a5b4f9f
dfd1123 <dfd1123@naver.com>
2024-08-01 17:49:16 +0900

    Storybook: FeedDetailEditor에 hash list data 적용

--- storybook/stories/FeedDetailEditor.stories.tsx
+++ storybook/stories/FeedDetailEditor.stories.tsx
@@ -42,13 +42,13 @@ export const Default: StoryComponent = {
       },
       hash: {
         list: [
-          // { id: 1, name: 'Derek' },
-          // { id: 2, name: 'Easton' },
-          // { id: 3, name: 'Easton' },
-          // { id: 4, name: 'Easton' },
-          // { id: 5, name: 'Easton' },
-          // { id: 6, name: 'Easton' },
-          // { id: 7, name: 'Easton' }
+          { id: 1, name: 'Derek' },
+          { id: 2, name: 'Easton' },
+          { id: 3, name: 'Easton' },
+          { id: 4, name: 'Easton' },
+          { id: 5, name: 'Easton' },
+          { id: 6, name: 'Easton' },
+          { id: 7, name: 'Easton' }
         ],
         onCompleteHash({ allHashTag, currentHashTag }) {
           console.log(allHashTag);
