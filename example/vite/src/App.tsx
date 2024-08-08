e826610c52a4293a4d1ad246a9df6e9fff7140f1
koo-ji-young <110871105+koojiyoung@users.noreply.github.com>
2024-08-08 21:29:25 +0900

    test

--- example/vite/src/App.tsx
+++ example/vite/src/App.tsx
@@ -21,7 +21,7 @@ function App() {
 					<SvgReact />
 				</a>
 			</div>
-			<h1>Vite + React</h1>
+			<h1>Vite + Reactasdas</h1>
 			<div className='card'>
 				<button onClick={() => {
 					setCount(count => count + 1);
