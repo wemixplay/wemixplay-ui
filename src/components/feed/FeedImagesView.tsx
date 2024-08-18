702185a438b7d54e2d509fab50a1549426344968
dfd1123 <dfd1123@naver.com>
2024-08-18 14:50:16 +0900

    fix: image 로딩 UI 적용 및 이미지 추가 시 자동 슬라이드  조정

--- src/components/feed/FeedImagesView.tsx
+++ src/components/feed/FeedImagesView.tsx
@@ -1,14 +1,15 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import style from './FeedImagesView.module.scss';
 import Carousel from '../carousel/Carousel';
 import { SvgIcoDeleteX } from '@/assets/svgs';
 import { makeCxFunc } from '@/utils/forReactUtils';
+import Spinner from '../loadings/Spinner';
 
 type Props = {
   className?: string;
-  images?: { file?: File; src: string }[];
+  images?: { file?: File; loading?: boolean; src: string }[];
   handleDeleteImg?: ({ deleteIndex }: { deleteIndex: number }) => void;
   handleClickImage?: ({ src, index }: { src: string; index: number }) => void;
 };
@@ -21,8 +22,7 @@ const FeedImagesView = ({
   handleDeleteImg,
   handleClickImage
 }: Props) => {
-  //logic
-
+  const curentImagesRef = useRef(images);
   return (
     <div className={cx(className, 'images-upload-preview')}>
       <Carousel
@@ -31,6 +31,15 @@ const FeedImagesView = ({
         loop={false}
         slidesPerView={'auto'}
         spaceBetween={10}
+        onSlidesLengthChange={(swiper) => {
+          if (curentImagesRef.current.length < images.length) {
+            setTimeout(() => {
+              swiper.slideTo(images.length - 1, 200);
+            }, 200);
+          }
+
+          curentImagesRef.current = images;
+        }}
       >
         {images.map((image, index) => (
           <div key={`${image.src}-${index}`}>
@@ -48,6 +57,11 @@ const FeedImagesView = ({
               className={cx('preview-image-box', { 'has-click-event': handleClickImage })}
               onClick={() => handleClickImage && handleClickImage({ src: image.src, index })}
             >
+              {!!image.loading && (
+                <div className={cx('loading')}>
+                  <Spinner />
+                </div>
+              )}
               <img src={image.src} alt={image.src} />
             </div>
           </div>
