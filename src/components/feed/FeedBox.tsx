c69d79f7db41c09a76ec11330cc0b167930f3824
dfd1123 <dfd1123@naver.com>
2024-08-28 15:05:18 +0900

    Feat: 뷰포트에 보였을때 유튜브 영상 자동 실행/정지 되도록 수정

--- src/components/feed/FeedBox.tsx
+++ src/components/feed/FeedBox.tsx
@@ -1,7 +1,15 @@
 'use client';
 
-import React, { MouseEvent, ReactElement, useCallback, useId } from 'react';
-import classNames from 'classnames/bind';
+import React, {
+  forwardRef,
+  MouseEvent,
+  MutableRefObject,
+  ReactElement,
+  useCallback,
+  useId,
+  useImperativeHandle,
+  useRef
+} from 'react';
 import style from './FeedBox.module.scss';
 import FeedImagesView, { FeedImagesViewProps } from './FeedImagesView';
 import PopoverButton from '../popover/PopoverButton';
@@ -11,7 +19,7 @@ import FeedEtcInfoArea from './FeedEtcInfoArea';
 import FeedWriterInfo from './FeedWriterInfo';
 import FeedTextContent from './FeedTextContent';
 import FeedLinkPreview, { FeedLinkPreviewProps } from './FeedLinkPreview';
-import FeedIframesView from './FeedIframesView';
+import FeedIframesView, { FeedIframesViewRef } from './FeedIframesView';
 import { makeCxFunc } from '@/utils/forReactUtils';
 
 type Props = {
@@ -32,6 +40,7 @@ type Props = {
   likeCount?: number;
   isMyLike?: boolean;
   textEllipsis?: boolean;
+  intersectionVideo?: boolean;
   createdAt?: number;
   updatedAt?: number;
   locale?: string;
@@ -48,142 +57,171 @@ type Props = {
   onProfileClick?: (e: MouseEvent<HTMLElement>) => void;
 };
 
-const cx = makeCxFunc(style);
-
-const FeedBox = ({
-  className = '',
-  writerName,
-  writerImg,
-  categoryName,
-  certificated,
-  textContent,
-  images = [],
-  media = [],
-  ogMetaData,
-  managePopoverElement = <></>,
-  emojiSelectPopoverElement = <></>,
-  emojiList = [],
-  commentCount = 0,
-  likeCount = 0,
-  isMyLike,
-  avatarSize = 36,
-  createdAt,
-  updatedAt,
-  locale = 'en',
-  textEllipsis = true,
-  onClick,
-  onMentionClick,
-  onHashTagClick,
-  onManageBtnClick,
-  onEmojiSelectBtnClick,
-  onEmojiClick,
-  onShareBtnClick,
-  onImageClick,
-  onLikeBtnClick,
-  onCommentBtnClick,
-  onProfileClick
-}: Props) => {
-  const uid = useId();
-
-  const handleProfileClick = useCallback(
-    (e: MouseEvent<HTMLElement>) => {
-      e.stopPropagation();
-
-      onProfileClick && onProfileClick(e);
-    },
-    [onProfileClick]
-  );
-
-  const handleManageBtnClick = useCallback(
-    (e: MouseEvent<HTMLButtonElement>) => {
-      e.stopPropagation();
-
-      onManageBtnClick && onManageBtnClick(e);
-    },
-    [onManageBtnClick]
-  );
-
-  return (
-    <article
-      className={cx(className, 'feed-box', { 'has-click-event': onClick })}
-      onClick={onClick}
-    >
-      <div className={cx('feed-box-container')}>
-        <div className={cx('feed-header')}>
-          <FeedWriterInfo
-            className={cx('profile')}
-            name={writerName}
-            profileImg={writerImg}
-            profileSize={avatarSize}
-            categoryName={categoryName}
-            certificated={certificated}
-            createdAt={createdAt}
-            locale={locale}
-            onProfileClick={handleProfileClick}
-          />
-
-          {/* Feed Management Button (삭제, 수정, 신고...) */}
-          {!!managePopoverElement || !!onManageBtnClick ? (
-            <div className={cx('btn-manage')}>
-              <PopoverButton
-                anchorId={onManageBtnClick ? '' : `feed-manage-${uid.replace(/:/gi, '')}`}
-                id={`feed-manage-${uid.replace(/:/gi, '')}`}
-                popoverStyle={{ right: -10, top: 10, zIndex: 999 }}
-                popoverElement={managePopoverElement}
-                popoverAnimation={{ name: 'modal-pop-fade', duration: 300 }}
-                whenWindowScrollClose={true}
-                onClick={handleManageBtnClick}
-              >
-                <SvgIcoVDots width={22} height={22} />
-              </PopoverButton>
-            </div>
-          ) : (
-            <></>
-          )}
-        </div>
-        <div className={cx('feed-body')}>
-          <FeedTextContent
-            className={cx('text-content')}
-            content={textContent}
-            ellipsis={textEllipsis}
-            onTextClick={onClick}
-            onMentionClick={onMentionClick}
-            onHashTagClick={onHashTagClick}
-          />
-
-          {images.length > 0 && (
-            <FeedImagesView
-              className={cx('carousel')}
-              images={images.map((src) => ({ src: src }))}
-              onImageClick={onImageClick}
-            />
-          )}
-          {media.length > 0 && <FeedIframesView className={cx('carousel')} media={media} />}
-          {!!ogMetaData && <FeedLinkPreview ogMetaData={ogMetaData} />}
-        </div>
-
-        <div className={cx('feed-footer')}>
-          <FeedEmojiArea
-            className={cx('feed-reactions')}
-            emojiList={emojiList}
-            emojiSelectPopoverElement={emojiSelectPopoverElement}
-            onEmojiClick={onEmojiClick}
-            onEmojiSelectBtnClick={onEmojiSelectBtnClick}
-          />
-          <FeedEtcInfoArea
-            className={cx('feed-comments')}
-            commentCount={commentCount}
-            likeCount={likeCount}
-            isMyClick={isMyLike}
-            onLikeBtnClick={onLikeBtnClick}
-            onShareBtnClick={onShareBtnClick}
-            onCommentBtnClick={onCommentBtnClick}
-          />
-        </div>
-      </div>
-    </article>
-  );
+type FeedBoxRef = HTMLElement & {
+  iframeRef: FeedIframesViewRef;
 };
 
-export type { Props as FeedBoxProps };
+const cx = makeCxFunc(style);
+
+const FeedBox = forwardRef<FeedBoxRef, Props>(
+  (
+    {
+      className = '',
+      writerName,
+      writerImg,
+      categoryName,
+      certificated,
+      textContent,
+      images = [],
+      media = [],
+      ogMetaData,
+      managePopoverElement = <></>,
+      emojiSelectPopoverElement = <></>,
+      emojiList = [],
+      commentCount = 0,
+      likeCount = 0,
+      isMyLike,
+      avatarSize = 36,
+      intersectionVideo,
+      createdAt,
+      updatedAt,
+      locale = 'en',
+      textEllipsis = true,
+      onClick,
+      onMentionClick,
+      onHashTagClick,
+      onManageBtnClick,
+      onEmojiSelectBtnClick,
+      onEmojiClick,
+      onShareBtnClick,
+      onImageClick,
+      onLikeBtnClick,
+      onCommentBtnClick,
+      onProfileClick
+    },
+    ref
+  ) => {
+    const uid = useId();
+
+    const elRef = useRef<FeedBoxRef>();
+    const iframeRef = useRef<FeedIframesViewRef>();
+
+    const handleProfileClick = useCallback(
+      (e: MouseEvent<HTMLElement>) => {
+        e.stopPropagation();
+
+        onProfileClick && onProfileClick(e);
+      },
+      [onProfileClick]
+    );
+
+    const handleManageBtnClick = useCallback(
+      (e: MouseEvent<HTMLButtonElement>) => {
+        e.stopPropagation();
+
+        onManageBtnClick && onManageBtnClick(e);
+      },
+      [onManageBtnClick]
+    );
+
+    useImperativeHandle(ref, () => {
+      elRef.current.iframeRef = iframeRef.current;
+
+      return elRef.current;
+    });
+
+    return (
+      <article
+        ref={elRef}
+        className={cx(className, 'feed-box', { 'has-click-event': onClick })}
+        onClick={onClick}
+      >
+        <div className={cx('feed-box-container')}>
+          <div className={cx('feed-header')}>
+            <FeedWriterInfo
+              className={cx('profile')}
+              name={writerName}
+              profileImg={writerImg}
+              profileSize={avatarSize}
+              categoryName={categoryName}
+              certificated={certificated}
+              createdAt={createdAt}
+              locale={locale}
+              onProfileClick={handleProfileClick}
+            />
+
+            {/* Feed Management Button (삭제, 수정, 신고...) */}
+            {!!managePopoverElement || !!onManageBtnClick ? (
+              <div className={cx('btn-manage')}>
+                <PopoverButton
+                  anchorId={onManageBtnClick ? '' : `feed-manage-${uid.replace(/:/gi, '')}`}
+                  id={`feed-manage-${uid.replace(/:/gi, '')}`}
+                  popoverStyle={{ right: -10, top: 10, zIndex: 999 }}
+                  popoverElement={managePopoverElement}
+                  popoverAnimation={{ name: 'modal-pop-fade', duration: 300 }}
+                  whenWindowScrollClose={true}
+                  onClick={handleManageBtnClick}
+                >
+                  <SvgIcoVDots width={22} height={22} />
+                </PopoverButton>
+              </div>
+            ) : (
+              <></>
+            )}
+          </div>
+          <div className={cx('feed-body')}>
+            <FeedTextContent
+              className={cx('text-content')}
+              content={textContent}
+              ellipsis={textEllipsis}
+              onTextClick={onClick}
+              onMentionClick={onMentionClick}
+              onHashTagClick={onHashTagClick}
+            />
+
+            {images.length > 0 && (
+              <FeedImagesView
+                className={cx('carousel')}
+                images={images.map((src) => ({ src: src }))}
+                onImageClick={onImageClick}
+              />
+            )}
+            {media.length > 0 && (
+              <FeedIframesView
+                ref={iframeRef}
+                className={cx('carousel')}
+                media={media}
+                intersectionVideo={intersectionVideo}
+              />
+            )}
+            {!!ogMetaData && <FeedLinkPreview ogMetaData={ogMetaData} />}
+          </div>
+
+          <div className={cx('feed-footer')}>
+            <FeedEmojiArea
+              className={cx('feed-reactions')}
+              emojiList={emojiList}
+              emojiSelectPopoverElement={emojiSelectPopoverElement}
+              onEmojiClick={onEmojiClick}
+              onEmojiSelectBtnClick={onEmojiSelectBtnClick}
+            />
+            <FeedEtcInfoArea
+              className={cx('feed-comments')}
+              commentCount={commentCount}
+              likeCount={likeCount}
+              isMyClick={isMyLike}
+              onLikeBtnClick={onLikeBtnClick}
+              onShareBtnClick={onShareBtnClick}
+              onCommentBtnClick={onCommentBtnClick}
+            />
+          </div>
+        </div>
+      </article>
+    );
+  }
+);
+
+FeedBox.displayName = 'FeedBox';
+
+export type { Props as FeedBoxProps, FeedBoxRef };
 export default FeedBox;
--- src/components/feed/FeedIframesView.tsx
+++ src/components/feed/FeedIframesView.tsx
@@ -1,49 +1,183 @@
 'use client';
 
-import React, { useCallback, useRef, MouseEvent, useState, useEffect } from 'react';
+import React, {
+  useCallback,
+  useRef,
+  MouseEvent,
+  useState,
+  useEffect,
+  forwardRef,
+  useImperativeHandle,
+  MutableRefObject,
+  useMemo,
+  HTMLAttributes
+} from 'react';
 import style from './FeedIframesView.module.scss';
 import Carousel from '../carousel/Carousel';
 import { SvgIcoDeleteX } from '@/assets/svgs';
 import { makeCxFunc } from '@/utils/forReactUtils';
+import YouTube, { YouTubeEvent, YouTubePlayer, YouTubeProps } from 'react-youtube';
 
 type Props = {
   className?: string;
   media?: { type?: 'youtube' | 'twitch'; src?: string }[];
+  intersectionVideo?: boolean;
   handleDeleteIframe?: ({ deleteIndex }: { deleteIndex: number }) => void;
+};
+
+export type IframePlayState = 'NOT_YET' | 'ENDED' | 'PLAYING' | 'PAUSED' | 'BUFFERING' | 'CUED';
+
+export type FeedIframesViewRef = HTMLDivElement & {
+  playState: IframePlayState;
+  playVideo: () => void;
+  pauseVideo: () => void;
 };
 
 const cx = makeCxFunc(style);
 
-const FeedIframesView = ({ className = '', media = [], handleDeleteIframe }: Props) => {
-  return (
-    <div className={cx(className, 'feed-iframe-views')}>
-      <Carousel
-        className={cx('iframe-preview-slider')}
-        freeMode
-        loop={false}
-        slidesPerView={'auto'}
-        spaceBetween={10}
-      >
-        {media.map((iframe, index) => (
-          <div key={`${iframe.src}-${index}`}>
-            {!!handleDeleteIframe && (
-              <button
-                className={cx('btn-img-delete')}
-                onClick={() => handleDeleteIframe({ deleteIndex: index })}
+const FeedIframesView = forwardRef<FeedIframesViewRef, Props>(
+  ({ className = '', media = [], intersectionVideo, handleDeleteIframe }, ref) => {
+    const youtubeRef = useRef<YouTubePlayer>();
+    const iframePreviewBoxRef = useRef<FeedIframesViewRef>();
+
+    const [playState, setPlayState] = useState<IframePlayState>('NOT_YET');
+    const [ready, setReady] = useState(false);
+
+    const io = useMemo(() => {
+      if (typeof window === 'undefined' || !intersectionVideo) {
+        return;
+      }
+
+      if (window.IntersectionObserver) {
+        return new IntersectionObserver(
+          (entries) => {
+            if (entries[0]?.intersectionRatio > 0.8) {
+              youtubeRef.current.playVideo();
+            } else {
+              youtubeRef.current.pauseVideo();
+            }
+          },
+          {
+            threshold: [0, 0.25, 0.5, 0.75, 1.0]
+          }
+        );
+      }
+    }, [intersectionVideo]);
+
+    const getYoutubeVideoId = useCallback((src: string) => {
+      const [url] = src.split('?');
+
+      return url.split('/').pop();
+    }, []);
+
+    const handleYoutubePlayStateChange = useCallback((event: YouTubeEvent<number>) => {
+      if (event.data === -1) {
+        setPlayState('NOT_YET');
+      } else if (event.data === 0) {
+        setPlayState('ENDED');
+      } else if (event.data === 1) {
+        setPlayState('PLAYING');
+      } else if (event.data === 2) {
+        setPlayState('PAUSED');
+      } else if (event.data === 3) {
+        setPlayState('BUFFERING');
+      } else if (event.data === 5) {
+        setPlayState('CUED');
+      }
+    }, []);
+
+    const playVideo = useCallback(() => {
+      if (youtubeRef.current) {
+        youtubeRef.current.playVideo();
+      }
+    }, []);
+
+    const pauseVideo = useCallback(() => {
+      if (youtubeRef.current) {
+        youtubeRef.current.pauseVideo();
+      }
+    }, []);
+
+    useEffect(() => {
+      const el = iframePreviewBoxRef.current;
+
+      if (io && ready) {
+        io.observe(el);
+
+        return () => {
+          io.disconnect();
+        };
+      }
+    }, [ready]);
+
+    useImperativeHandle(ref, () => {
+      iframePreviewBoxRef.current.playState = playState;
+      iframePreviewBoxRef.current.playVideo = playVideo;
+      iframePreviewBoxRef.current.pauseVideo = pauseVideo;
+
+      return iframePreviewBoxRef.current;
+    });
+
+    return (
+      <div className={cx(className, 'feed-iframe-views')}>
+        <Carousel
+          className={cx('iframe-preview-slider')}
+          freeMode
+          loop={false}
+          slidesPerView={'auto'}
+          spaceBetween={10}
+        >
+          {media.map((iframe, index) => (
+            <div key={`${iframe.src}-${index}`}>
+              {!!handleDeleteIframe && (
+                <button
+                  className={cx('btn-img-delete')}
+                  onClick={() => handleDeleteIframe({ deleteIndex: index })}
+                >
+                  <SvgIcoDeleteX />
+                </button>
+              )}
+              <div
+                ref={iframePreviewBoxRef}
+                key={iframe.src}
+                data-src={iframe.src}
+                className={cx('preview-iframe-box', playState)}
               >
-                <SvgIcoDeleteX />
-              </button>
-            )}
-            <div key={iframe.src} className={cx('preview-iframe-box')}>
-              <span className={cx('swipe-helper', 'before')}></span>
-              <span className={cx('swipe-helper', 'after')}></span>
-              <iframe src={iframe.src} />
+                {/* <span className={cx('swipe-helper', 'before')}></span>
+                <span className={cx('swipe-helper', 'after')}></span> */}
+                {iframe.type === 'youtube' && (
+                  <YouTube
+                    videoId={getYoutubeVideoId(iframe.src)}
+                    opts={
+                      {
+                        playerVars: {
+                          autoplay: 0,
+                          rel: 0,
+                          controls: 1,
+                          modestbranding: 1,
+                          loop: 1,
+                          enablejsapi: 1,
+                          origin: 'https://www.youtube.com'
+                        }
+                      } as YouTubeProps['opts']
+                    }
+                    onReady={(e) => {
+                      youtubeRef.current = e.target;
+                      setReady(true);
+                    }}
+                    onStateChange={handleYoutubePlayStateChange}
+                  />
+                )}
+                {iframe.type === 'twitch' && <iframe src={iframe.src} />}
+              </div>
             </div>
-          </div>
-        ))}
-      </Carousel>
-    </div>
-  );
-};
+          ))}
+        </Carousel>
+      </div>
+    );
+  }
+);
+
+FeedIframesView.displayName = 'FeedIframesView';
 
 export default FeedIframesView;
