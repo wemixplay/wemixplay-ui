f3b457c04197c7cad03a310964c57ab9399c4a8c
dfd1123 <dfd1123@naver.com>
2024-09-25 14:44:06 +0900

    Docs: 컴포넌트 설명 주석 추가

--- src/plugins/mention/components/MentionList.tsx
+++ src/plugins/mention/components/MentionList.tsx
@@ -15,29 +15,66 @@ import { isDesktop } from 'react-device-detect';
 import style from './MentionList.module.scss';
 import { makeCxFunc } from '@/utils/forReactUtils';
 import Person from '@/components/avatars/Person';
-import WpImage from '@/components/image/WpImage';
 import { SvgIcoCertified } from '@/assets/svgs';
 import { MentionInfo } from '../Mention';
 import useCheckDevice from '@/hooks/useCheckDevice';
 
 type MentionListRef = HTMLDivElement & {
+  /** 위쪽 방향으로 선택 항목을 이동시키는 함수 */
   handleArrowUp: () => void;
+  /** 아래쪽 방향으로 선택 항목을 이동시키는 함수 */
   handleArrowDown: () => void;
+  /**
+   * 현재 선택된 멘션을 제출하는 함수
+   * @param {number} [index] - 선택된 멘션의 인덱스, 선택되지 않으면 현재 focus된 항목을 제출
+   * @returns {MentionInfo} 선택된 멘션 정보
+   */
   handleSubmit: (index?: number) => MentionInfo;
 };
 
 type Props = {
+  /** 추가적인 CSS 클래스명 */
   className?: string;
+  /** 멘션 목록 */
   list?: MentionInfo[];
+  /** 선택할 멘션의 ID */
   targetMentionId?: string;
+  /** 멘션이 적용될 콘텐츠 편집 영역의 참조 객체 */
   contentEditableEl: MutableRefObject<HTMLDivElement>;
-  listElement?: (item: JSONObject) => React.JSX.Element;
+  /**
+   * 멘션 목록 아이템을 커스텀 렌더링하는 함수
+   * @param {MentionInfo} item - 멘션 정보 객체
+   * @returns {React.JSX.Element} 렌더링할 멘션 리스트 아이템
+   */
+  listElement?: (item: MentionInfo) => React.JSX.Element;
+  /**
+   * 멘션 아이템을 선택했을 때 호출되는 콜백 함수
+   * @param {number} index - 선택된 멘션 아이템의 인덱스
+   */
   selectMentionItem: (index?: number) => void;
+  /**
+   * 멘션 목록이 닫힐때 호출되는 함수
+   */
   closeMentionList: () => void;
 };
 
 const cx = makeCxFunc(style);
 
+/**
+ * `MentionList` 컴포넌트는 멘션 목록을 표시하고 사용자가 선택할 수 있도록 돕는 UI 컴포넌트입니다.
+ * 멘션 목록은 사용자가 멘션를 입력할 때 자동 완성 제안으로 사용됩니다.
+ * 목록에서 선택한 멘션은 외부에서 지정된 콜백 함수로 처리됩니다.
+ *
+ * @component
+ * @example
+ * <MentionList
+ *   list={mentions}
+ *   targetMentionId="target-mention-id"
+ *   contentEditableEl={contentEditableRef}
+ *   selectMentionItem={handleSelectMentionItem}
+ *   closeMentionList={handleCloseMentionList}
+ * />
+ */
 const MentionList = forwardRef<MentionListRef, Props>(
   (
     {
@@ -286,7 +323,7 @@ const MentionList = forwardRef<MentionListRef, Props>(
     return (
       <div
         ref={elRef}
-        className={cx('mention-list')}
+        className={cx(className, 'mention-list')}
         contentEditable={false}
         style={{ ...position, display: (list ?? []).length > 0 ? 'block' : 'none' }}
       >
--- src/plugins/mention/components/MentionPortal.tsx
+++ src/plugins/mention/components/MentionPortal.tsx
@@ -4,9 +4,23 @@ import React, { ReactNode, useEffect, useState } from 'react';
 import { createPortal } from 'react-dom';
 
 type Props = {
+  /** 포탈을 통해 렌더링할 자식 컴포넌트들 */
   children: ReactNode;
 };
 
+/**
+ * `MentionPortal` 컴포넌트는 `ReactDOM.createPortal`을 사용하여 특정 DOM 노드에 자식 컴포넌트를 렌더링합니다.
+ * 기본적으로 `#wp-editor-mention-portal`이라는 `div` 요소를 만들어 그 안에 자식을 렌더링하며,
+ * 해당 DOM 노드가 없을 경우, 시스템이 직접 생성하여 문서의 body에 추가합니다.
+ *
+ * 컴포넌트가 언마운트될 때 시스템이 생성한 DOM 요소는 자동으로 제거됩니다.
+ *
+ * @component
+ * @example
+ * <MentionPortal>
+ *   <div>멘션 목록 내용</div>
+ * </MentionPortal>
+ */
 const MentionPortal = ({ children }: Props) => {
   const [wrapperElement, setWrapperElement] = useState<HTMLElement | null>(null);
 
