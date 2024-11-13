78021143f494c65f8bf52bfdd05fff5f62e594a7
sonny <sonny@wemade.com>
2024-11-13 18:13:35 +0900

    feat: CurationBox 추가

--- storybook/stories/CurationBox.stories.tsx
+++ storybook/stories/CurationBox.stories.tsx
@@ -25,19 +25,7 @@ export const Default: StoryComponent = {
   args: {
     createdAt: Date.now(),
     locale: 'ko',
-    intersectionVideo: true,
-    textContent: 'Commit Message',
-    // images: [
-    //   'https://cdn.pixabay.com/animation/2022/10/11/09/05/09-05-26-529_512.gif',
-    //   'https://cdn-images-1.medium.com/fit/t/1600/480/1*AmI9wRbXrfIWGESx6eEiTw.gif'
-    // ],
-    // media: [
-    //   {
-    //     src: 'https://player.twitch.tv/?channel=dyushi&autoplay=false&muted=false&parent=localhost',
-    //     type: 'twitch'
-    //   }
-    // ],
-    hideFooter: true
+    textContent: 'Commit Message'
   },
   render: Template
 };
