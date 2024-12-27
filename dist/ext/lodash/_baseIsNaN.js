c7a14375fc11d26a8bc1d50505075c0d4783329c
dfd1123 <dfd1123@naver.com>
2024-12-27 16:22:21 +0900

    update version to 1.13.164-alpha

---
+++ dist/ext/lodash/_baseIsNaN.js
@@ -0,0 +1,1 @@
+"use strict";var r=function(r){return r!=r};exports._baseIsNaN=r;
---
+++ dist/plugins/mention/components/MentionContainer.js
@@ -0,0 +1,1 @@
+"use strict";var e=require("react"),t=require("./MentionPortal.js");module.exports=n=>{let{mention:r,children:o}=n;const[i,l]=e.useState(r.mentionId),[s,u]=e.useState(r.config);return e.useEffect((()=>{(null==r?void 0:r.observe)&&r.observe({setTargetMentionId:l,setConfig:u})}),[r]),i?e.createElement(t,null,o({config:s,targetMentionId:i})):e.createElement(e.Fragment,null)};
