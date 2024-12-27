78633d7791769b1c21680c74435911de00216cb6
dfd1123 <dfd1123@naver.com>
2024-12-27 11:57:30 +0900

    fix: webpack-svg-component-plugin 웹팩 훅 설정 변경

--- src/modules/svg-component-generator/webpack/webpack-svg-component-plugin.ts
+++ src/modules/svg-component-generator/webpack/webpack-svg-component-plugin.ts
@@ -1,33 +1,22 @@
 import chokidar from 'chokidar';
 import path from 'path';
 import SvgComponentGenerator, { type SvgComponentGeneratorOption } from '../svgComponentGenerator';
+import type { Compiler } from 'webpack';
 
 type WebpackPluginOptions = SvgComponentGeneratorOption & {
   // Types
 };
 
-type Compiler = {
-  hooks: {
-    emit: {
-      tap: (name: string, callback: (stats: unknown) => void) => void;
-    };
-    done: {
-      tap: (name: string, callback: (stats: unknown) => void) => void;
-    };
-  };
-  assets: JSONObject;
-};
-
 class WebpackSvgComponentPlugin {
-  private readonly svgCompGenertor: SvgComponentGenerator;
+  private svgCompGenerator: SvgComponentGenerator;
   private readonly svgFileDir: string;
   private readonly outputDir: string;
   private watcher?: chokidar.FSWatcher;
 
   constructor({ svgFileDir, outputDir, typescript, title, description }: WebpackPluginOptions) {
     this.svgFileDir = path.join(process.cwd(), svgFileDir);
-    this.outputDir = outputDir;
-    this.svgCompGenertor = new SvgComponentGenerator({
+    this.outputDir = outputDir ?? '';
+    this.svgCompGenerator = new SvgComponentGenerator({
       svgFileDir,
       outputDir,
       typescript,
@@ -36,30 +25,42 @@ class WebpackSvgComponentPlugin {
     });
   }
 
+  generate() {
+    this.svgCompGenerator.generate();
+  }
+
   async apply(compiler: Compiler) {
-    compiler.hooks.emit.tap('SvgComponentGeneratorPlugin', (_stats) => {
-      void this.svgCompGenertor.generate();
+    compiler.hooks.compile.tap('WebpackSvgComponentPlugin', () => {
+      if (process.env.NODE_ENV === 'development') {
+        if (!this.watcher) {
+          // Watcher가 이미 존재하지 않는 경우에만 생성
+          this.watcher = chokidar.watch(this.svgFileDir, {
+            persistent: true,
+            ignored: /\/svg\/types\//
+          });
+
+          this.watcher.on('add', this.generate);
+          this.watcher.on('unlink', this.generate);
+        }
+      } else {
+        void this.svgCompGenerator.generate();
+      }
+
+      process.once('SIGINT', () => {
+        if (this.watcher) {
+          this.watcher.close();
+        }
+
+        process.exit(0);
+      });
     });
 
-    if (process.env.NODE_ENV === 'development') {
-      if (!this.watcher) {
-        this.watcher = chokidar.watch(this.svgFileDir, {
-          persistent: true,
-          ignored: [new RegExp(this.outputDir), /\/svg\/types\//] // outputDir을 감시 대상에서 제외
-        });
-
-        this.watcher.on('add', () => this.svgCompGenertor.generate());
-        this.watcher.on('unlink', () => this.svgCompGenertor.generate());
-
-        process.once('SIGINT', () => {
-          if (this.watcher) {
-            this.watcher.close();
-          }
-
-          process.exit(0);
-        });
+    compiler.hooks.done.tap('WebpackSvgComponentPlugin', () => {
+      if (this.watcher) {
+        this.watcher.close();
+        this.watcher = null;
       }
-    }
+    });
   }
 }
 
