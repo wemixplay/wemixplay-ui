0b7f89491788acdc20c9ed8f17b962a07a652a02
dfd1123 <dfd1123@naver.com>
2025-01-02 19:07:50 +0900

    feat: WemixplayUIProvider 추가

--- src/providers/WemixplayUi/index.tsx
+++ src/providers/WemixplayUi/index.tsx
@@ -1,21 +1,32 @@
 import React, { createContext } from 'react';
 import styles from './wemixplay-ui.module.scss';
-import clsx from 'clsx';
+import { makeCxFunc } from '@/utils/forReactUtils';
 
-const cx = clsx(styles);
+const cx = makeCxFunc(styles);
 
-type WemixplayUIContext = {
-  //
+type WemixplayUIContextType = {
+  /** wemixplay ui 현재 테마 */
+  theme?: 'dark' | 'light';
 };
 
 type WemixplayUIProviderProps = {
+  /** wemixplay ui 현재 테마 */
+  theme?: 'dark' | 'light';
   children: React.ReactNode;
 };
 
-const WemixplayUIContext = createContext<WemixplayUIContext>({});
+const WemixplayUIContext = createContext<WemixplayUIContextType>({});
 
-const WemixplayUIProvider = ({ children }: WemixplayUIProviderProps) => {
-  return <WemixplayUIContext.Provider value={{}}>{children}</WemixplayUIContext.Provider>;
+const WemixplayUIProvider = ({ children, theme }: WemixplayUIProviderProps) => {
+  return (
+    <WemixplayUIContext.Provider value={{ theme }}>
+      <div className={cx('wemixplay-ui')} data-theme={theme}>
+        {children}
+      </div>
+    </WemixplayUIContext.Provider>
+  );
 };
 
+export type { WemixplayUIContextType };
+export { WemixplayUIContext };
 export default WemixplayUIProvider;
