5367148a74f7c43a34e19c5583ec05f65c8898cb
dfd1123 <dfd1123@naver.com>
2025-01-02 16:49:16 +0900

    update version to 1.13.185-alpha

---
+++ dist/esm/ext/lodash/isObjectLike.js
@@ -0,0 +1,1 @@
+var t=function(t){return null!=t&&"object"==typeof t};export{t as i};
---
+++ dist/plugins/hashTag/components/HashContainer.js
@@ -0,0 +1,2 @@
+'use client';
+"use strict";var e=require("react"),t=require("./HashPortal.js");module.exports=r=>{let{hash:s,children:a}=r;const[l,n]=e.useState(s.hashId),[u,o]=e.useState(s.config);return e.useEffect((()=>{(null==s?void 0:s.observe)&&s.observe({setTargetHashId:n,setConfig:o})}),[s]),l?e.createElement(t,null,a({config:u,targetHashId:l})):e.createElement(e.Fragment,null)};
