7d567b6d7557181d166a14f07b711502a138a778
dfd1123 <dfd1123@naver.com>
2025-01-02 15:22:28 +0900

    fix: BulletList의 스타일성 props 추가

--- src/components/bulletList/BulletList.tsx
+++ src/components/bulletList/BulletList.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, ReactNode } from 'react';
+import React, { CSSProperties, ReactElement, ReactNode } from 'react';
 
 import { makeCxFunc } from '@/utils/forReactUtils';
 
@@ -8,18 +8,55 @@ const cx = makeCxFunc(style);
 
 interface Props {
   className?: string;
+  /**
+   * 리스트 항목들. React 요소 또는 문자열 배열.
+   */
   list: (ReactElement | string)[] | ReactNode[];
-  icon?: ReactElement | string;
+  /**
+   * 불릿 아이콘. React 요소 또는 문자열.
+   */
+  bulletIcon?: ReactElement | string;
+  /**
+   * 불릿과 텍스트 사이의 간격.
+   */
+  bulletGap?: CSSProperties['paddingLeft'];
+  /**
+   * 불릿 아이콘의 top 및 left 위치 값.
+   */
+  bulletIconPosition?: {
+    top?: CSSProperties['top'];
+    left?: CSSProperties['left'];
+  };
 }
 
-const BulletList = ({ className = '', list, icon }: Props) => {
+/**
+ * BulletList 컴포넌트는 불릿 아이콘과 함께 리스트 항목을 렌더링합니다.
+ *
+ * @component
+ * @param {string} [props.className=''] - 추가적인 CSS 클래스 이름. 기본값은 빈 문자열.
+ * @param {(ReactElement | string)[] | ReactNode[]} props.list - 리스트 항목들. React 요소 또는 문자열 배열.
+ * @param {ReactElement | string} [props.bulletIcon] - 커스텀 불릿 아이콘. React 요소 또는 문자열.
+ * @param {CSSProperties['paddingLeft']} [props.bulletGap] - 불릿과 텍스트 사이의 간격.
+ * @param {{ top?: CSSProperties['top']; left?: CSSProperties['left']; }} [props.bulletIconPosition] - 불릿 아이콘의 top 및 left 위치 값.
+ */
+const BulletList = ({
+  className = '',
+  list,
+  bulletIcon,
+  bulletGap,
+  bulletIconPosition = {}
+}: Props) => {
   //logic
 
   return (
-    <ul className={cx('bullet-list', className, { 'default-circle': !icon })}>
+    <ul className={cx('bullet-list', className, { 'default-circle': !bulletIcon })}>
       {list?.map((item, index) => (
-        <li key={index}>
-          {icon && <i className={cx('bullet-icon')}>{icon}</i>}
+        <li key={index} style={{ paddingLeft: bulletGap }}>
+          {bulletIcon && (
+            <i className={cx('bullet-icon')} style={bulletIconPosition}>
+              {bulletIcon}
+            </i>
+          )}
           {item}
         </li>
       ))}
