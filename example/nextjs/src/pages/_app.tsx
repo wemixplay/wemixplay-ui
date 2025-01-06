87533e125e6aa7ce7fb1124b2775fafaea55dcdb
dfd1123 <dfd1123@naver.com>
2025-01-06 12:51:05 +0900

    fix: nextjs 예제 wemixplay-ui 버전 업데이트 후 테스트

--- example/nextjs/src/pages/_app.tsx
+++ example/nextjs/src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { WemixplayUIProvider } from "wemixplay-ui";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <WemixplayUIProvider><Component {...pageProps} /></WemixplayUIProvider>;
 }
