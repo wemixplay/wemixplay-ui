7f51ff04d6ea71d52f17c87ec12eec34148beca8
wemixplay <dapp@wemade.com>
2025-01-06 04:48:27 +0000

    cleanup dist

--- dist/esm/constants/components/tab/ChipTab.d.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { TabProps } from './Tab';
-type Props = TabProps & {};
-declare const ChipTab: {
-    ({ className, ...tabProps }: Props): React.JSX.Element;
-    displayName: string;
-};
-export type { Props as ChipTabProps };
-export default ChipTab;
--- dist/ext/lodash/_objectToString.js
+++
@@ -1,1 +0,0 @@
-"use strict";var t=Object.prototype.toString;var r=function(r){return t.call(r)};exports._objectToString=r;
