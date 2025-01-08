b3d01514d67a500c7dbc49fd05062315e849db44
dfd1123 <dfd1123@naver.com>
2025-01-08 18:19:37 +0900

    update version to 1.14.8

---
+++ dist/esm/components/clientOnly/ClientOnly.js
@@ -0,0 +1,2 @@
+"use client";
+import e,{useState as t,useEffect as r}from"react";const l=l=>{let{children:a,fallback:n=""}=l;const[c,o]=t(!1);return r((()=>{o(!0)}),[]),e.createElement(e.Fragment,null,c?a:n)};export{l as default};
