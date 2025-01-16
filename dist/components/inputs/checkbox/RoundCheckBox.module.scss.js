2d3bc68b70c78b4bf6785a520af0cd32b8be8efa
wemixplay <dapp@wemade.com>
2025-01-16 08:20:09 +0000

    cleanup dist

--- dist/components/inputs/checkbox/RoundCheckBox.module.scss.js
+++
@@ -1,1 +0,0 @@
-"use strict";require('./../../../ext/style-inject/dist/style-inject.es.js')(".RoundCheckBox-module_checkbox-round__joLjj .ico-check .icon{border-radius:50%;height:20px;width:20px}",{insertAt:"top"}),module.exports={"checkbox-round":"RoundCheckBox-module_checkbox-round__joLjj"};
