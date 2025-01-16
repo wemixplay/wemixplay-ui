7c064791b5bdb640fd1967c06dbad686ecd75444
wemixplay <dapp@wemade.com>
2025-01-16 06:35:34 +0000

    cleanup dist

--- dist/esm/ext/lodash/_toSource.js
+++
@@ -1,1 +0,0 @@
-var t=Function.prototype.toString;var r=function(r){if(null!=r){try{return t.call(r)}catch(t){}try{return r+""}catch(t){}}return""};export{r as _};
--- dist/esm/ext/prop-types/lib/ReactPropTypesSecret.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-var _="SECRET_DO_NOT_PASS_THIS_OR_YOU_WILL_BE_FIRED";export{_ as R};
