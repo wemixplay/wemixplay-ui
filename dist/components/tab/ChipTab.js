c8a3b310d73983c46cfea433c164f95f7dfafc77
sonny <sonny@wemade.com>
2025-01-20 11:49:53 +0900

    update version to 1.13.233-alpha

---
+++ dist/components/tab/ChipTab.js
@@ -0,0 +1,2 @@
+"use client";
+"use strict";var e=require("../../_virtual/_tslib.js"),s=require("react"),r=require("../../utils/forReactUtils.js"),a=require("./ChipTab.module.scss.js"),t=require("./Tab.js");const i=r.makeCxFunc(a),c=r=>{var{className:a=""}=r,c=e.__rest(r,["className"]);return s.createElement(t,Object.assign({className:i(a,"chip-tab")},c))};c.displayName="ChipTab";var u=c;module.exports=u;
