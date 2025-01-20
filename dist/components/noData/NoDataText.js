514a3c958dc8b1b720d6e885c1ce92bec131fc4c
dfd1123 <dfd1123@naver.com>
2025-01-20 20:02:49 +0900

    update version to 1.14.13

---
+++ dist/components/noData/NoDataText.js
@@ -0,0 +1,1 @@
+"use strict";var e=require("react"),t=require("../../utils/forReactUtils.js"),a=require("./NoData.module.scss.js");const s=t.makeCxFunc(a);var r=t=>{let{className:a,nullText:r="no data",style:l={paddingTop:8}}=t;return e.createElement("div",{className:s("no-data",a),style:l},e.createElement("p",{className:s("no-data-text")},r))};module.exports=r;
