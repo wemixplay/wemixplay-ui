5b6678d98b790cf65f989f0348987cda556feb86
elena1 <elena1@wemade.com>
2025-04-23 14:59:32 +0900

    update version to 1.13.246-alpha

---
+++ dist/esm/ext/lodash/_getPrototype.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_overArg.js";var t=r(Object.getPrototypeOf,Object);export{t as _};
---
+++ dist/ext/lodash/_getPrototype.js
@@ -0,0 +1,1 @@
+"use strict";var e=(0,require("./_overArg.js")._overArg)(Object.getPrototypeOf,Object);exports._getPrototype=e;
